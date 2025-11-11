38bd0da3899b230ec900ba68c32754c5d0a9b78f
rifathosenofficial2023 <rifathosenofficial@gmail.com>
2025-11-11 22:39:44 +0600

    first selenium code

---
+++ selenium_explore_01.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import time
+
+service = Service("D:\Python\Selenium\chromedriver.exe")
+driver = webdriver.Chrome(service=service)
+
+driver.get("https://www.facebook.com")
+time.sleep(10)
